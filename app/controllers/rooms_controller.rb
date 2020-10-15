27994bf2802304916f156f405018e1add2005510
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-10-15 11:58:15 +0200

    card wip

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,10 @@
 class RoomsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
     @rooms = Room.all
+  end
+
+  def show
   end
 end
