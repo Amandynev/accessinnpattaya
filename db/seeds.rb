35d957be8946500dd53f628b06aba441294df34c
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-10-15 11:17:10 +0200

    seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ accessinn = Category.create!(
   description: "Discover the fantastic refreshing and unique Access inn room. Located on the 2nd and 3rd floor of the hotel. Panoramic Lift access. Music system in room, special LED design and a spacious room with a sofa corner and a balcony with his PVC sliding window as a basement of your personal relaxation during your stay.",
   price: 800,
   room_size: 29,
-  bed_size: 1,
+  bed_size: "King size",
   balcony: true,
   corner_table: true
 )
@@ -26,7 +26,7 @@ superior = Category.create!(
   description: "Discover the fantastic refreshing superior rooms. Located on the 3 floors of the hotel. Panoramic Lift access and Music system in room as a basement of your personal relaxation during your stay.",
   price: 490,
   room_size: 18,
-  bed_size: 1,
+  bed_size: "Queen size",
   balcony: false,
   corner_table: false
 )
@@ -36,7 +36,7 @@ deluxe = Category.create!(
   description: "Discover the fantastic refreshing deluxe rooms. Located on the 3 floors of the hotel. Panoramic Lift access. Music system in room and a private balcony with his PVC sliding window as a basement of your personal relaxation during your stay.",
   price: 550,
   room_size: 19,
-  bed_size: 1,
+  bed_size: "King size",
   balcony: true,
   corner_table: false
 )
@@ -46,7 +46,7 @@ premium = Category.create!(
   description: "Discover the fantastic refreshing premium rooms. Located on the 3 floors of the hotel. Panoramic Lift access. Music system in room, special LED design and a private balcony with his PVC sliding window as a basement of your personal relaxation during your stay.",
   price: 650,
   room_size: 25,
-  bed_size: 1,
+  bed_size: "King size",
   balcony: true,
   corner_table: true
 )
@@ -56,30 +56,30 @@ suite = Category.create!(
   description: "Discover the fantastic refreshing suite rooms. Located on the 1st floor of the hotel. Panoramic Lift access. Music system in room, special LED design and a spacious room with a sofa corner and a private balcony with his PVC sliding window as a basement of your personal relaxation during your stay.",
   price: 750,
   room_size: 27,
-  bed_size: 1,
+  bed_size: "King size",
   balcony: true,
   corner_table: true
 )
 
 puts "create rooms..."
 
-6.times do
+5.times do
   Room.create!(category: accessinn)
 end
 
-1.times do
+3.times do
   Room.create!(category: superior)
 end
 
-1.times do
+6.times do
   Room.create!(category: deluxe)
 end
 
-1.times do
+11.times do
   Room.create!(category: premium)
 end
 
-1.times do
+2.times do
   Room.create!(category: suite)
 end
 
