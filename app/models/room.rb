f69cbbb630dfb4ac6ed0592a36157f239f47c316
Maxence Lenoir <maxence.lenoir1206@gmail.com>
2020-10-15 15:00:34 +0200

    database changed

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :category
-  has_many :bookings
+  has_many :room_bookings
 
   SERVICES = ['open 24h/24h', 'wifi', 'air condition', 'minibar', 'room service', 'laundry', 'restaurant', 'bar', 'massage']
 
