75cdf6305881fa6b9acaea543943859a0c3a9580
Maxence Lenoir <maxence.lenoir1206@gmail.com>
2020-10-17 15:56:58 +0200

    little change

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,6 @@
 class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
-    @room = BikeOffer.find(params[:bike_offer_id])
-
-    @booking.bike_offer =  @bike_offer
-    @booking.user = current_user
-
-    if @booking.save
-      redirect_to all_my_bookings_path
-    else
-      render bike_offer(@bike_offer)
-    end
   end
 
   private
