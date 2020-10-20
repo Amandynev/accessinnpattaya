d65598c9d62f967250a60d71a060a4a8e84079fa
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-10-20 22:32:53 +0200

    setup navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,23 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :restaurant, :event, :contact_us]
 
   def home
+      @categories = Category.all
+
+  end
+
+  def restaurant
+  end
+
+  def event
+  end
+
+  def contact
     @markers =
       {
         lat: Hotel.first.latitude,
         lng: Hotel.first.longitude
       }
-
-      @categories = Category.all
-
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'contact', to: 'pages#home'
+  get 'restaurant', to: 'pages#home'
+  get 'event', to: 'pages#home'
   resources :rooms, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
