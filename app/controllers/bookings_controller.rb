18706c82189ec55ba86d0ac6ee2e418463b80c4d
Maxence Lenoir <maxence.lenoir1206@gmail.com>
2020-10-21 09:08:00 +0200

    mergin issue

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,7 @@
 class BookingsController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
+    p room_available
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     if @booking.save
@@ -14,5 +15,11 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:start_at, :end_at, :user_id)
+  end
+
+  def room_available
+    category = @room.category
+    rooms = Room.where(category: category)
+    @bookings = Booking.where(room: rooms)
   end
 end
