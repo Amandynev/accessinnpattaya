0b020b10e40ed46abb84e0a8e0661b46489124ed
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-10-21 10:45:20 +0200

    new colonne photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_115514) do
+ActiveRecord::Schema.define(version: 2020_10_21_084441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_115514) do
 
   create_table "rooms", force: :cascade do |t|
     t.bigint "category_id", null: false
+    t.string "photo"
     t.index ["category_id"], name: "index_rooms_on_category_id"
   end
 
