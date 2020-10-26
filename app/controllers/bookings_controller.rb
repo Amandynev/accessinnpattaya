5b66cc578819c41f2c7f7925ee5828114fd2efda
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-10-26 17:07:41 +0100

    Refonte

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,16 @@
 class BookingsController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
-    room_available
-    @booking = Booking.new(booking_params)
-    @booking.room = @room
-    @booking.user = current_user
-    if @booking.save
-      RoomBooking.create(booking: @booking, room: @room)
-      redirect_to rooms_path
+    @booking = Booking.new
+    if room_available
+      @booking = Booking.new(booking_params)
+      @booking.room = @room
+      @booking.user = current_user
+      if @booking.save
+        redirect_to rooms_path
+      else
+        render "rooms/show"
+      end
     else
       render "rooms/show"
     end
@@ -22,15 +25,17 @@ class BookingsController < ApplicationController
   def room_available
     category = @room.category
     rooms = Room.where(category: category)
-    @bookings = Booking.where(rooms: rooms)
+    @bookings = Booking.where(room: rooms)
     rooms.each do |room|
+      binding.pry
+      return true if room.bookings.empty?
       room.bookings.each do |booking|
-        date_range = booking.start_at...booking.end_at
+        date_range = booking.start_at..booking.end_at
         start_date = Date.new(params[:booking]["start_at(1i)"].to_i, params[:booking]["start_at(2i)"].to_i, params[:booking]["start_at(3i)"].to_i)
         end_date = Date.new(params[:booking]["end_at(1i)"].to_i, params[:booking]["end_at(2i)"].to_i, params[:booking]["end_at(3i)"].to_i)
-        return false if date_range.include?(start_date) || date_range.include?(end_date)
+        return true unless date_range.include?(start_date) || date_range.include?(end_date) || (start_date < booking.start_at && end_at > booking.end_at)
       end
     end
-    true
+    false
   end
 end
