41015fa0098f092819e18b639b499c9a70c12ebc
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-10-28 12:49:43 +0100

    Enlever les puts

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,17 +1,10 @@
 class StripeCheckoutSessionService
   def call(event)
-    p "==================DEBUT===================="
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
-    p "======================MILIEU======================="
     bookings = Booking.where("user_id = ? AND state = ?", order.user, "pending")
-    p bookings
     bookings.each do |booking|
-      p "=================UPDATE==================="
-      p booking
       booking.update(state: 'paid')
-      p booking
     end
-    p "======================FIN======================="
   end
 end
