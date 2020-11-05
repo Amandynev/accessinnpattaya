634b1f64a4056635abee1afc852deb6f3cd14875
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-11-05 16:24:06 +0100

    Front Card Booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -23,9 +23,11 @@ class BookingsController < ApplicationController
 
   def allmybookings
     @bookings = Booking.where("user_id = ? AND state = ?", current_user, "pending")
-    @hash_bookings = Hash.new { |hash, key| hash[key] = { number: 0, room: nil } }
+    @hash_bookings = Hash.new { |hash, key| hash[key] = { number: 0, room: nil, nigths: 0, price: 0 } }
     @bookings.each do |booking|
       @hash_bookings[booking.room.category.name][:number] += 1
+      @hash_bookings[booking.room.category.name][:nigths] += (booking.end_at - booking.start_at).to_i
+      @hash_bookings[booking.room.category.name][:price] += booking.price / 100
       @hash_bookings[booking.room.category.name][:room] = booking.room if @hash_bookings[booking.room.category.name][:room].nil?
     end
     @amount = @bookings.map(&:price).sum
