411a1dcbfdbfd478e52075a36a73ff0da74e2291
Maxence Lenoir <maxence.lenoir1206@gmail.com>
2020-11-11 13:01:22 +0100

    Test view bookings

---
+++ spec/features/bookings_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "Bookings", type: :feature do
+  let!(:user) { create(:user) }
+
+  describe 'Bookings Views' do
+    it 'Visit allmybookings page' do
+      Booking.destroy_all
+      login_as user
+      create(:booking, user: user)
+      visit(allmybookings_path)
+      expect(page).to have_current_path(allmybookings_path)
+      expect(page).to have_css('.card-booking')
+    end
+  end
+end
