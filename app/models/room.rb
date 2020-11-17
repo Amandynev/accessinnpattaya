a8d4cddb19082f11e5bfd154cf59d842fb14907e
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-11-17 14:09:13 +0100

    margin room services

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,7 @@ class Room < ApplicationRecord
   belongs_to :category
   has_many :bookings
 
-  SERVICES = ['Safety box', 'Wifi', 'Air condition', 'Minibar', 'Lift', 'Shower room', 'Room service', 'Laundry service', 'Parking']
+  SERVICES = ['Safety-box', 'Wifi', 'Air-condition', 'Minibar', 'Lift', 'Shower room', 'Room-service', 'Laundry service', 'Parking']
 
   def services
     SERVICES
