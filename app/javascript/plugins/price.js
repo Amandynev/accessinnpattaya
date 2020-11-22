277e550bd6693e8357df17cf367df2e2b68b9ffe
Maxence Lenoir <maxence.lenoir1206@gmail.com>
2020-11-22 15:14:21 +0100

    Prise en compte nombre de chambre pour JS affichage prix

--- app/javascript/plugins/price.js
+++ app/javascript/plugins/price.js
@@ -1,17 +1,20 @@
 const price = () => {
   const priceBox = document.querySelector('.price')
   if (priceBox) {
+    const inputs = document.querySelectorAll('input')
     const priceRoom = priceBox.dataset.price;
     const startInput = document.querySelector('#range_start');
     const endInput = document.querySelector('#range_end');
-    startInput.addEventListener('change', () => {
-      priceBox.innerText = ""
-      const priceDisplay = ((new Date(endInput.value) - new Date(startInput.value)) / 86400000) * priceRoom;
-      if (!isNaN(priceDisplay)) {
-        console.log('coucou')
-        priceBox.insertAdjacentText('beforeend', priceDisplay)
-      }
-    });
+    const numberInput = document.querySelector("#booking_number");
+    inputs.forEach(input => {
+      input.addEventListener('change', () => {
+        priceBox.innerText = "" 
+        const priceDisplay = ((new Date(endInput.value) - new Date(startInput.value)) / 86400000) * priceRoom * numberInput.value;
+        if (!isNaN(priceDisplay)) {
+          priceBox.insertAdjacentText('beforeend', priceDisplay)
+        }
+      });
+    })
   }
 }
 
