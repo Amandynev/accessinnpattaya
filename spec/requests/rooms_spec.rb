204eb68b571a6a8ab43890cea05bef51f2affb37
Maxence Lenoir <maxence.lenoir1206@gmail.com>
2020-11-28 11:59:44 +0100

    Tests search rooms

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Rooms", type: :request do
   let!(:locale) { nil }
-  let!(:room) {Category.all.sample.rooms.first}
+  let!(:accessinn) { Category.find_by(name: "The Access inn") }
+  let!(:rooms) { Room.where(category: accessinn) }
+  Booking.destroy_all
 
   describe "As a visitor" do
     it 'Should access the rooms index' do
@@ -11,12 +13,15 @@ RSpec.describe "Rooms", type: :request do
     end
 
     it 'Should access a room show' do
-      get room_path(locale, room)
+      get room_path(locale, rooms.first)
       expect(response).to have_http_status '200'
     end
 
-    # it 'Should be able to check availability' do
-
-    # end
+    it 'Should be able to check availability' do
+      build(:booking, room: rooms.first)
+      build(:booking, room: rooms.second)
+      get searchedrooms_path params: { start_at: [Date.today.to_s], end_at: [(Date.today + 2).to_s] }
+      expect(response).to have_http_status '200'
+    end
   end
 end
