d48aebe54622fd1c0d6803f274ba94246e226616
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2020-12-01 12:20:14 +0100

    Test mail review

--- spec/requests/contacts_request_spec.rb
+++ spec/requests/contacts_request_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe "Pages", type: :request do
   let(:page) { Page.new(name: Faker::FunnyName.name, email: Faker::Internet.email, message: Faker::Lorem.paragraph, nickname: Faker::FunnyName.name) }
 
   it 'Send an e-mail when valid data' do
-    expect { page.deliver }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    mail = HotelMailer.with(name: page.name, phone: page.phone, email: page.email, message: page.message).contact
+    expect { mail.deliver }.to change { ActionMailer::Base.deliveries.count }.by(1)
   end
 
   it 'Do not send an e-mail with invalid data' do
