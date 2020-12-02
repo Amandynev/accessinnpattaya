413a86b4b5b1997fb1dbc56c5b4859a3ac0346b2
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2020-12-02 21:02:01 +0100

    Mail proprietaire Ok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-  config.hosts << "ad0c5a23d151.ngrok.io"
+  config.hosts << "9bd91e817c7e.ngrok.io"
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
