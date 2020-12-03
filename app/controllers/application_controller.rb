6c109f0607c686a5d41a1414e21744b38feea6e4
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2020-12-03 10:39:41 +0100

    Change user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :set_locale
   add_flash_types :sent, :booked, :not_booked
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name last_name phone adress city_code country])
+  end
 
   def set_locale
     I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
