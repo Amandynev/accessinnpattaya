fc29b535a7876f71cb6543192cdbb5e08dc21bfd
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-12-08 00:01:55 +0100

    emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.default_url_options = { host: "http://accessinnpattaya.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -62,12 +62,12 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "accessinnpattaya_production"
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-config.action_mailer.default_url_options = { :host => 'www.accessinnpattaya.com' }
+config.action_mailer.default_url_options = { host: 'accessinnpattaya.herokuapp.com' }
 # config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.postmarkapp.com',
   port:                 587,
-  domain:               'yourdomaine.com',
+  domain:               'herokuapp.com',
   user_name:             ENV["POSTMARK_API_TOKEN"],
   password:              ENV["POSTMARK_API_TOKEN"],
   authentication:       :plain,
